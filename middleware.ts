9bcb70064da9ccc82d68eebd24f78e5488c40dc5
Comunicacao <comunicacao@maisbr.org.br>
2026-04-23 12:43:34 -0300

    fix: corrigido o middleware para não tratar a api como página logada

--- middleware.ts
+++ middleware.ts
@@ -8,12 +8,11 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
+     * Protege todas as rotas, EXCETO:
+     * - api/cron (para permitir os Cron Jobs da Vercel)
+     * - arquivos estáticos do Next.js (_next/static, _next/image)
+     * - favicon e extensões de imagem comuns
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api/cron|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
